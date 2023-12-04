76f4a63b4af65c58a0ceeede34a5e617e081846c
Rosya-edwica <yunoshev.dev@gmail.com>
2023-12-05 00:01:06 +0300

    добавил класс Transcriber который может транскрибировать видео с помощью гугл и openai

--- src/audio.py
+++ src/audio.py
@@ -1,73 +1,85 @@
-import speech_recognition as sr
 import os
+import re
+import math
 import shutil
+from typing import Literal
+
+import speech_recognition as sr
 from pydub import AudioSegment
-from pydub.silence import split_on_silence
-from time import perf_counter
+
+import gpt
 
 CHUNKS_DIR = "data/chunks/"
 TEXT_DIR = "data/text/"
 
-def transcribe_large_audio(audio_path: str) -> str:
-    """
-    Input: audio_path format .wav
-    Output: text
-    """
-    text = check_result_for_audio_exists(audio_path)
-    if text:
+
+class Transcriber:
+    def __init__(self, audio_path: str = None):
+        self.Recognizer = sr.Recognizer()
+        self.audio_path = audio_path
+        if self.audio_path:
+            self.main_audio = AudioSegment.from_wav(self.audio_path)
+        self.text = ""
+
+    def set_new_audio_path(self, path: str):
+        self.audio_path = path
+        if self.audio_path.endswith(".wav"):
+            self.main_audio = AudioSegment.from_wav(self.audio_path)
+        elif self.audio_path.endswith(".mp3"):
+            self.main_audio = AudioSegment.from_mp3(self.audio_path)
+
+    def clean_cache(self):
+        shutil.rmtree(CHUNKS_DIR)
+    
+    def transcribe(self, min_per_split: int, method: list[Literal["whisher", "google"]]) -> str:
+        if self.audio_path is None:
+            exit("Забыли передать аудиофайл в class Transcribe")
+        
+        sound_mins = math.ceil(self.main_audio.duration_seconds / 60)
+        os.makedirs(name=CHUNKS_DIR, exist_ok=True) 
+        for i in range(0, sound_mins, min_per_split):
+            chunk_filename = os.path.join(CHUNKS_DIR, f"chunk_{i}.wav")
+            match method:
+                case "whisher":
+                    chunk_text = self.__get_text_for_chunk_by_whisher(chunk_filename, i, i+min_per_split)
+                case "google":
+                    chunk_text = self.__get_text_for_chunk_by_google(chunk_filename, i, i+min_per_split)
+            if chunk_text:
+                self.text += chunk_text
+        
+        self.save_text()
+        self.clean_cache()
+        return self.text
+    
+    def __get_text_for_chunk_by_whisher(self, chunk_path: str, from_min: int, to_min: int):
+        start = from_min * 60 * 1000
+        end = to_min * 60 * 1000
+        chunk_audio = self.main_audio[start:end]
+        chunk_audio.export(chunk_path, format="mp3")
+        text = gpt.transcribe_audio(chunk_path)
+        print(chunk_path, ":", text)
         return text
 
-    start = perf_counter()
-    sound = AudioSegment.from_wav(audio_path)
-    # Дробим аудио на мелкие части
-    chunks = split_on_silence(sound, min_silence_len=700, silence_thresh=sound.dBFS-14, keep_silence=700)
-    
-    # Создаем временную папку, куда будем сохранять кусочки аудио
-    os.makedirs(name=CHUNKS_DIR, exist_ok=True) 
-    r = sr.Recognizer()    
-    text_result = ""
-    for i, audio_chunk in enumerate(chunks, start=1):
-        # Создаем кусочек и сохраняем его в файлик
-        chunk_filename = os.path.join(CHUNKS_DIR, f"chunk{i}.wav")
-        audio_chunk.export(chunk_filename, format="wav")
-
-        # Пытаемся прочитать наш кусочек аудио
-        with sr.AudioFile(chunk_filename) as source:
-            audio_listened = r.record(source)
+    def __get_text_for_chunk_by_google(self, chunk_path: str, from_min: int, to_min: int) -> str | None:
+        start = from_min * 60 * 1000
+        end = to_min * 60 * 1000
+        chunk_audio = self.main_audio[start:end]
+        chunk_audio.export(chunk_path, format="wav")
+        with sr.AudioFile(chunk_path) as source:
+            audio_listened = self.Recognizer.record(source)
             try:
-                text = r.recognize_google(audio_listened, language="ru-RU")
+                text = self.Recognizer.recognize_google(audio_listened, language="ru-RU")
             except sr.UnknownValueError as e:
                 print("Error:", str(e))
+                return None
             else:
-                print(chunk_filename, ":", text)
-                text_result += text
-   
-    # Сохраняем результат, чтобы в следующий раз не тратить много времени
-    save_text(audio_path, text_result)
-    # Удаляем временную папку
-    shutil.rmtree(CHUNKS_DIR)
-    print(f"Время на транскрибацию аудио: {perf_counter() - start} сек.")
-    return text_result
+                print(chunk_path, ":", text)
+                return text
 
-def check_result_for_audio_exists(audio_path: str) -> str | None:
-    """
-    Транскрибация длинного аудио занимает много времени, поэтому нам лучше лишний раз проверить
-    не сохраняли ли мы раньше результат по нашему аудио
-    """
-    audio_name = audio_path.split("/")[-1]
-    text_path = os.path.join(TEXT_DIR, audio_name.replace(".wav", ".txt"))
-    if os.path.exists(text_path):
-        text_file = open(text_path, mode="r", encoding="utf-8")
-        text = text_file.read()
+    def save_text(self):
+        os.makedirs(name=TEXT_DIR, exist_ok=True)
+        audio_name = self.audio_path.split("/")[-1]
+        text_path = os.path.join(TEXT_DIR, audio_name.replace(".wav", ".txt"))
+        text_file = open(text_path, encoding="utf-8", mode="w")
+        text_file.write(self.text)
         text_file.close()
-        return text
-    
-    return None
-
-def save_text(audio_path: str, text: str) -> None:
-    os.makedirs(name=TEXT_DIR, exist_ok=True)
-    audio_name = audio_path.split("/")[-1]
-    text_path = os.path.join(TEXT_DIR, audio_name.replace(".wav", ".txt"))
-    text_file = open(text_path, encoding="utf-8", mode="w")
-    text_file.write(text)
-    text_file.close()
