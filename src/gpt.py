7123d1f2ef9259c89f837017f17a4c7240a4f311
Rosya-edwica <yunoshev.dev@gmail.com>
2023-12-05 00:03:28 +0300

    create_test_by_text принимает отдельно промт и текст для более гибкой настройки запросов. transcribe_audio() использует whisper-1 для транскрибации с помощью openai

--- src/gpt.py
+++ src/gpt.py
@@ -1,52 +1,71 @@
-import openai
+from openai import OpenAI
 from config import Answer, get_gpt_request_cost
 import os
 from dotenv import load_dotenv
+
 
 loaded_env = load_dotenv("../.env")
 if not load_dotenv:
     exit("Создайте файл с переменными окружения в корневой директории .env")
 
 GPT_TOKEN = os.getenv("GPT_TOKEN")
-GPT_MODEL = "gpt-3.5-turbo-16k-0613"
+GPT_MODEL = "gpt-4-1106-preview"
 ANSWERS_FOLDER = "data/answers/"
 
-def get_answer_from_gpt(query: str) -> Answer | str:
-    openai.api_key = GPT_TOKEN
+
+def create_test_by_text(query: str, text: str) -> Answer | None:
+    """
+    query: Промт для получения теста
+    text: Текст по которому нужно составить тест
+    """
+
+    client = OpenAI(api_key=GPT_TOKEN)
     try:
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model=GPT_MODEL,
             temperature=0,
             messages=[
                 {
-                    "role": "user",
+                    "role": "system",
                     "content": query,
                 },
+                {
+                    "role": "user",
+                    "content": text,
+                }
             ],
         )
     except BaseException as err:
-        return f"Не удалось подключиться к GPT. Текст ошибки: {err}"
+        print(f"Не удалось подключиться к GPT. Текст ошибки: {err}")
+        return
 
     question_tokens = response.usage.prompt_tokens
     answer_tokens = response.usage.completion_tokens
     return Answer(
         Question=query,
-        Text=[i["message"]["content"] for i in response["choices"]][0],
+        Text=response.choices[0].message.content,
         AnswerTokens=answer_tokens,
         QuestionTokens=question_tokens,
         Cost=get_gpt_request_cost(question_tokens, answer_tokens)
     )
 
-def get_test_for_text(text: str) -> Answer | None:
-    question = f"Мне нужно проверить своих студентов на знание вот этого текста: `{text}`\nСоставь тест из 10 вопросов с вариантами ответов в такой структуре: 1. Вопрос\na) первый вариант\nb) второй вариант\nc) третий вариант\nd) четвертый вариант\nОтвет: полный вариант"
-    answer = get_answer_from_gpt(question)
-    if isinstance(answer, Answer):
-        return answer
 
-    print(f"GPT error: {answer}")
-    return None
+def transcribe_audio(path: str) -> str:
+    """
+    transcribe_audio() с помощью API openAI достает текст из .mp3-аудиофайла
+    """
+    audio_file = open(path, mode="rb")
+    client = OpenAI(api_key=GPT_TOKEN)
+    answer = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+    return answer.text
+
 
 def save_answer(text: str, filename: str):
+    """Сохраняем ответ пока в файл, потом будем сохранять в бд"""
+    
     os.makedirs(ANSWERS_FOLDER, exist_ok=True)
     file = open(os.path.join(ANSWERS_FOLDER, filename), mode="r", encoding="utf-8")
     file.write(text)
