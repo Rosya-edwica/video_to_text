74fa2c74f2a6afe479cce4460ad8757cd682846d
Rosya-edwica <yunoshev.dev@gmail.com>
2023-12-05 00:02:12 +0300

    video_to_audio теперь выдает файл либо в mp3, либо в wav  формате

--- src/video.py
+++ src/video.py
@@ -1,21 +1,24 @@
 from moviepy.editor import VideoFileClip
+from typing import Literal
 import os
 
 AUDIO_DIR = "data/audio/"
 
-def video_to_audio(video_path: str) -> str:
+def video_to_audio(video_path: str, audio_format: list[Literal["mp3", "wav"]]):
     """
     Input: video_path .mp4 format
-    Output: audio_path .wav format
+    Output: audio_path .mp3/.wav format
     """
     os.makedirs(AUDIO_DIR, exist_ok=True)
+    if audio_format not in ("mp3", "wav"):
+        exit("Неправильный формат для аудиофайла. Вы можете выбрать только mp3 или wav") 
 
     # Загружаем видео
     video = VideoFileClip(video_path)
     # Достаем из видео аудио-дорожку 
-    audio_file = video.audio 
+    audio_file = video.audio
     # Получаем название файла видео без расширения и директорий
-    video_name = video_path.split("/")[-1].replace(".mp4", ".wav") 
+    video_name = video_path.split("/")[-1].replace(".mp4", "." + audio_format) 
      # Даем название аудиофайлу как у видео
     audio_path = os.path.join(AUDIO_DIR, video_name) 
     # Сохраняем аудио
